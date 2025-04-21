5ad7db6e16a00f3e7fb9774be97fe150f0aa85e0
sabbircse499 <sabbir472003@gmail.com>
2025-04-21 16:03:34 +0600

    drf

--- signapp/models.py
+++ signapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class  Custumuser(models.Model):
+    username=models.CharField(max_length=100,unique=True)
+    email=models.EmailField(unique=True)
+    password=models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.username
